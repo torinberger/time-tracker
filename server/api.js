fe413ce815e7c36d3c98be81683da6b06714d0f0
torinberger <torinberger@torinberger.com>
2020-06-30 18:40:47 +1000

    Database can make queries now

--- server/api.js
+++ server/api.js
@@ -3,4 +3,9 @@ const router = require('koa-joi-router');
 const database = require('./database');
 const api = router();
 
+database.query('SELECT * FROM appuser', (err, res) => {
+  console.log(err ? err.stack : res.rows);
+  database.end();
+})
+
 module.exports = api;
